612fef2ef5e3066d353569d03bf15a72dbb1fd5f
Abhishek Kumar <95577419+abhishek300830@users.noreply.github.com>
2023-04-11 14:40:42 +0530

    errors

--- 19-JS-Bitwise and regExp/regExp.js
+++ 19-JS-Bitwise and regExp/regExp.js
@@ -1,1 +1,15 @@
 // do it
+// regular Expression is used in searching and replace method
+
+// i incaseSensitivity
+// g global
+// m multiline
+
+// \d	Find a digit
+// \s	Find a whitespace character
+// \b	Find a match at the beginning of a word like this: \bWORD, or at the end of a word like this: WORD\b
+// \uxxxx	Find the Unicode character specified by the hexadecimal number xxxx
+
+// n+	Matches any string that contains at least one n
+// n*	Matches any string that contains zero or more occurrences of n
+// n?	Matches any string that contains zero or one occurrences of n
